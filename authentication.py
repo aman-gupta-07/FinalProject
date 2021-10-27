4e9a354f2856411f7f0d819a0d407ab40f4b187b
Aman Gupta <87823472+aman-gupta-07@users.noreply.github.com>
2021-10-27 17:27:31 +0530

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -1,15 +1,16 @@
 
 from twilio.rest import Client
+import os
 import random
-account_sid = 'AC3207c908a23b982eaf9d1cec872e3270'
-auth_token = '621d95e11c525fb44f1c74edc7132dd3'
+account_sid = os.getenv('ACCOUNT_SID')
+auth_token = os.getenv('AUTH_TOKEN')
 client = Client(account_sid, auth_token)
 otp = random.randrange(100000,999999)
 
 message = client.messages.create(
     body='Your OTP for login into result server is '+str(otp),
-    from_='+19842039780',
-    to='+917080253038'
+    from_='*****',
+    to=''****'
 )
 
 print(message.sid)
